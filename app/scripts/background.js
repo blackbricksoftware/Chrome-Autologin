777961f0b4c40c1ad94d0f8d1a9a73b4ed0cd0d5
David Hayes <david@blackbricksoftware.com>
2015-06-09 15:37:34 -0500

    add debug settings

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,20 +1,21 @@
 'use strict';
 
-var dom = '[href="http://blackbricksoftware.com/"]';
+var debug = false;
+var dom = '[href*="//blackbricksoftware.com"]';
 
 $(function(){
 	if ($(dom).length>0) {
-		console.log('clicking '+dom);
+		if (debug) console.log('clicking '+dom);
 		if ($(dom).is('a')) {
 			$(dom)[0].click();
 		} else {
 			$(dom).click();
 		}
 	} else {
-		console.log('not found '+dom);
+		if (debug) console.log('not found '+dom);
 	}
 	var refreshtime = ((Math.random()-0.5)*10+60)*1000;
-	console.log(refreshtime);
+	if (debug) console.log(refreshtime);
 	setTimeout(function(){
 		location.reload(true);
 	},refreshtime);
